a4eeeafb0d84113e5000b8b324d5631daef50b48
Gav Hensley <gavinrhensley@gmail.com>
2021-02-06 18:08:04 -0500

    made csproj into solution and added a guitar class

---
+++ Guitar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    class Guitar
+    {
+        //Properties
+        public bool InTune { get; set; }
+        public int StringWearAndTear { get; set; } 
+        public int NeedForMaintenance { get; set; }
+
+
+        //Constructors
+
+        //Methods
+    }
+}
