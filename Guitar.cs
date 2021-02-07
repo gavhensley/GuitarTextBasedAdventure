53072e9f99dda650a3c0c085a9313dd64032a1fd
Gav Hensley <gavinrhensley@gmail.com>
2021-02-07 12:30:04 -0500

    added properties and methods to guitar class and created visual for main menu, but menu doesn't function yet.

--- Guitar.cs
+++ Guitar.cs
@@ -4,16 +4,106 @@ using System.Text;
 
 namespace Choose_Your_Class
 {
-    class Guitar
+    public class Guitar
     {
         //Properties
+        public int DaysWithGuitar { get; set; }
+        public string TypeOfGuitar { get; set; }
         public bool InTune { get; set; }
         public int StringWearAndTear { get; set; } 
         public int NeedForMaintenance { get; set; }
+        public int SkillLevel { get; set; }
 
 
         //Constructors
+        public Guitar()
+        {
+            DaysWithGuitar = 1;
+            InTune = false;
+            StringWearAndTear = 0; //If it reaches 100, a string will break and the user will not be able to play
+            NeedForMaintenance = 0; //Let's say the user has to maintain the guitar every 30 days or so. Once this number reaches 30, the user will get a notification to get maintenance. The message will loop until the user takes the guitar in for repairs, aka GetMaintenance().
+            SkillLevel = 0;
+        }
+
 
         //Methods
+        public int GetDay()
+        {
+            return DaysWithGuitar;
+        }
+
+        public void SetType(string typeOfGuitar)
+        {
+            TypeOfGuitar = typeOfGuitar.ToLower();
+        }
+
+        public string GetTypeOfGuitar()
+        {
+            return TypeOfGuitar;
+        }
+
+        public bool InTuneStatus()
+        {
+            return InTune;
+        }
+
+        public void DeTune()
+        {
+            InTune = false;
+        }
+
+        public void Tune()
+        {
+            InTune = true;
+        }
+        
+        public int GetStringStatus()
+        {
+            return StringWearAndTear;
+        }
+
+        public void ChangeStrings()
+        {
+            StringWearAndTear = 0;
+        }
+
+        public int GetNeedForMaintenance()
+        {
+            return NeedForMaintenance;
+        }
+
+        public int GetSkillLevel()
+        {
+            return SkillLevel;
+        }
+
+        public void GuitarMaintenance()
+        {
+            NeedForMaintenance = 0;
+            SkillLevel -= 5;
+        }
+
+        public void Practice()
+        {
+            SkillLevel += 3;
+            StringWearAndTear += 10;
+            InTune = false;
+        }
+
+        public void Play()
+        {
+            SkillLevel += 2;
+            StringWearAndTear += 5;
+            InTune = false;
+        }
+
+        public void Time()
+        {
+            SkillLevel -= 1;
+            StringWearAndTear += 1;
+            NeedForMaintenance += 1;
+            DaysWithGuitar += 1;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace Choose_Your_Class
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Guitar Player Simulator!" );
+            Console.WriteLine("\nThis will give you some insight into the daily life of a guitar player.");
+            Console.WriteLine("We won't go too in depth, but this should give you an idea of what happens in the daily life of a guitar player from getting their first guitar.");
+
+            bool simulation = true;
+
+            Guitar myGuitar = new Guitar();
+
+            Console.WriteLine("\nWhat kind of guitar do you want? Steel string acoustic? Electric? Classical? Flamenco?");
+            myGuitar.SetType((Console.ReadLine()));
+            Console.Clear();
             
+            while (simulation is true)
+            Console.WriteLine($"It's Day: {myGuitar.GetDay()} with your {(myGuitar.GetTypeOfGuitar())} guitar! Let's check out the guitar and how you are doing!");
+            //stats 
+            Console.WriteLine($"\nGuitar is in Tune?: {myGuitar.InTuneStatus()}");
+            Console.WriteLine($"String wear and tear: {myGuitar.GetStringStatus()}/100");
+            Console.WriteLine($"Need for maintenance: {myGuitar.GetNeedForMaintenance()}/30");
+            Console.WriteLine($"Your skill level: {myGuitar.GetSkillLevel()}/Only you decide the limit");
+
+            Console.WriteLine("\n1. Tune guitar");
+            Console.WriteLine("2. Practice");
+            Console.WriteLine("3. Change Strings");
+            Console.WriteLine("4. Take the guitar in for maintenance");
+            Console.WriteLine("5. Play a concert (recommended Skill is 25)");
+            Console.WriteLine("6. End simulation");
+            
+
         }
     }
 }
